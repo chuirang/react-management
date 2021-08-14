05c9bde3d0e859817e64f963ea100daa8ba014b5
Wonki Lee <chuirang@naver.com>
2021-08-15 00:25:43 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '홍길동',
+  'birthday': '19240821',
+  'gender': '남자',
+  'job': '떠돌이'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '화랑',
+  'birthday': '19240821',
+  'gender': '남자',
+  'job': '어부'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '강남',
+  'birthday': '19240821',
+  'gender': '여자',
+  'job': '부자'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develope management system!</h2>
+    <div>
+      {
+        customers.map(c=>{
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
